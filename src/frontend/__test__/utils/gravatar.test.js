9e148bd5657f71522d86061058ab3d3a437e5e6a
JefferLindao <jefferlindao@gmail.com>
2020-10-05 21:22:17 -0500

    probando gravatar function

---
+++ src/frontend/__test__/utils/gravatar.test.js
@@ -0,0 +1,7 @@
+/*import gravatar from '../../utils/gravatar'
+
+test('Gravatar Function', () => {
+  const email = ''
+  const gravatarUrl = ''
+  expect(gravatarUrl).toEqual(gravatar(email))
+})*/
